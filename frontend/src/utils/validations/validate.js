806430c0572fafaf6e62ff0c537d0e9b6d0943e4
Soufiane Oumbarek <150067195+Oumbarek-Soufiane@users.noreply.github.com>
2025-02-16 16:49:05 +0100

    Delete frontend directory

--- frontend/src/utils/validations/validate.js
+++
@@ -1,9 +0,0 @@
-export const validateEmail = (email) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  };
-  
-  export const validateStringLength = (str, minLength) => {
-    return str.length >= minLength;
-  };
-  
